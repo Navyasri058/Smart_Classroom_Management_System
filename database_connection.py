8877420c47da7ae21cd433eca27003a35680141c
Navyasri058 <navyasrichappidi23@gmail.com>
2026-07-28 19:57:37 +0530

    Connecting to MongoDB Atlas

--- database_connection.py
+++ database_connection.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://Navya:22371-Cm-058@thundercluster.irbjytt.mongodb.net/?appName=Thundercluster")
 db = client["studentslist_db"]
-collection = db["students"]
+collection = db["Students"]
